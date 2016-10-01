6012cad16ca08a83950971ee15fe93263645ac2f
danesever <bresa86@yahoo.com>
2016-10-01 19:53:39 +0200

    uradjena klasa avion fixes #4

---
+++ src/main/java/model/Avion.java
@@ -0,0 +1,45 @@
+package model;
+
+import com.j256.ormlite.field.DatabaseField;
+
+public class Avion {
+    public static final String POLJE_OZNAKA = "oznaka";
+    public static final String POLJE_RASPON_KRILA = "raspon_krila";
+
+    @DatabaseField(generatedId = true)
+    private int id;
+    @DatabaseField(columnName = POLJE_OZNAKA, canBeNull = false)
+    private String oznaka;
+    @DatabaseField(columnName = POLJE_RASPON_KRILA, canBeNull = false)
+    private int rasponKrila;
+
+    public Avion(String oznaka, int rasponKrila) {
+        this.oznaka = oznaka;
+        this.rasponKrila = rasponKrila;
+    }
+
+    public String getOznaka() {
+        return oznaka;
+    }
+
+    public void setOznaka(String oznaka) {
+        this.oznaka = oznaka;
+    }
+
+    public int getRasponKrila() {
+        return rasponKrila;
+    }
+
+    public void setRasponKrila(int rasponKrila) {
+        this.rasponKrila = rasponKrila;
+    }
+
+    @Override
+    public String toString() {
+        return "Avion{" +
+                "id=" + id +
+                ", oznaka='" + oznaka + '\'' +
+                ", rasponKrila=" + rasponKrila +
+                '}';
+    }
+}
